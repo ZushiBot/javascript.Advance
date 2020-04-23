423f7657907be3e3d8a241ce68d5eadb9bdac0b7
Raihan Chowdhury <62661424+ZushiBot@users.noreply.github.com>
2020-04-23 15:54:03 +0600

    Create Templeted_string.js

---
+++ Templeted_string.js
@@ -0,0 +1,7 @@
+/*
+EM6 New Feature To Express Strings Easily.
+:::Syntax:::
+:::    ${variable}   :::    `Dynamic String`    :::
+*/
+let f_name='andru';
+var d_string=`Hello my friend ${f_name},Welcome! This is a new way to describe a String`;
