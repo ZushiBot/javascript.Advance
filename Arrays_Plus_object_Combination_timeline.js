4835c090d43c5fdc4883efb32d3cdf4dcbeedc66
ZushiBot <62661424+ZushiBot@users.noreply.github.com>
2020-04-23 15:09:37 +0600

    Timeline
    user-style
    timeline
    facebook

---
+++ Arrays_Plus_object_Combination_timeline.js
@@ -0,0 +1,22 @@
+var facebook_timeline=[
+    {
+        username: "raihan",
+        Profile_name:"Raihan Chowdhury",
+        time:"12-12-2021",
+        post:"Hi! Lorem , You Saved My life.Nice story by the way.",
+        likes:"20",
+        comments:[
+            "nice","wow","Great"
+        ]
+    },
+    {
+        username: "musfiq",
+        Profile_name:"Musfiq Chowdhury",
+        time:"13-12-2021",
+        post:"Did you ever read that story of Lorem?",
+        likes:"20",
+        comments:[
+            "Nope","Never","fact"
+        ]
+    }
+]
