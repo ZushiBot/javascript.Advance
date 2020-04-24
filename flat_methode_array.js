35ffa57ca51572b9a086bafb7acd3a446f34de89
Raihan Chowdhury <62661424+ZushiBot@users.noreply.github.com>
2020-04-24 15:56:32 +0600

    Array Methode

--- flat_methode_array.js
+++ flat_methode_array.js
@@ -1,2 +1,11 @@
-const array=[1,2,[3,4,5],10];
-array.flat();
+?*flat methode Will Flat That Array*/
+
+const array1 = [1,2,[3,3.1,[22,22,33],3.2],4];
+array1.flat();
+//Result => (7) [1, 2, 3, 3.1, Array(3), 3.2, 4]
+array1.flat(0);
+//Result => (4) [1, 2, Array(4), 4]
+array1.flat(1);
+//Result => (7) [1, 2, 3, 3.1, Array(3), 3.2, 4]
+array1.flat(2);
+//Result=> (9) [1, 2, 3, 3.1, 22, 22, 33, 3.2, 4]
