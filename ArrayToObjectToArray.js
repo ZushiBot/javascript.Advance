cc89413cb0ae2a857d4c4a4ae317d4cbd2d5a8df
Raihan Chowdhury <62661424+ZushiBot@users.noreply.github.com>
2020-04-24 16:17:17 +0600

    ArrayToObjectToArray

---
+++ ArrayToObjectToArray.js
@@ -0,0 +1,17 @@
+/*Array to object to array Using Object.fromEntries(array) And Object.entries(obj)
+
+const arr =[['raihan',22],['musfiq',21]];
+const obj = Object.fromEntries(arr);
+
+/*
+{raihan: 22, musfiq: 21}
+musfiq: 21
+raihan: 22
+*/
+
+Object.entries(obj);
+/*
+(2) [Array(2), Array(2)]
+0: (2) ["raihan", 22]
+1: (2) ["musfiq", 21]
+*/
